d69e7c64876a12aa347afae160def5366a9c8bf6
santhosh-c1 <102995631+santhosh-c1@users.noreply.github.com>
2025-03-31 22:07:14 +0530

    Adds demo action service (#35)
    * Adds demo action service
    
    * More actions and capabilities
    
    * Verion update of sdk
    
    * latest

---
+++ pkg/connector/actions.go
@@ -0,0 +1,116 @@
+package connector
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	config "github.com/conductorone/baton-sdk/pb/c1/config/v1"
+	v2 "github.com/conductorone/baton-sdk/pb/c1/connector/v2"
+	"github.com/conductorone/baton-sdk/pkg/actions"
+	"github.com/conductorone/baton-sdk/pkg/annotations"
+	"github.com/conductorone/baton-sdk/pkg/connectorbuilder"
+	"google.golang.org/protobuf/types/known/structpb"
+)
+
+var addNumbers = &v2.BatonActionSchema{
+	Name: "addNumbers",
+	Arguments: []*config.Field{
+		{
+			Name:        "number1",
+			DisplayName: "Number 1",
+			Field:       &config.Field_IntField{},
+			IsRequired:  true,
+		},
+		{
+			Name:        "number2",
+			DisplayName: "Number 2",
+			Field:       &config.Field_IntField{},
+			IsRequired:  true,
+		},
+	},
+	ReturnTypes: []*config.Field{
+		{
+			Name:        "sum",
+			DisplayName: "Sum",
+			Field:       &config.Field_IntField{},
+		},
+	},
+}
+
+var helloIn1Minute = &v2.BatonActionSchema{
+	Name: "helloIn1Minute",
+	Arguments: []*config.Field{
+		{
+			Name:        "name",
+			DisplayName: "Name",
+			Field:       &config.Field_StringField{},
+			IsRequired:  true,
+		},
+	},
+	ReturnTypes: []*config.Field{
+		{
+			Name:        "hello",
+			DisplayName: "Hello",
+			Field:       &config.Field_StringField{},
+		},
+	},
+}
+
+func (d *Demo) RegisterActionManager(ctx context.Context) (connectorbuilder.CustomActionManager, error) {
+	actionManager := actions.NewActionManager(ctx)
+
+	// addNumbers action
+	err := actionManager.RegisterAction(ctx, addNumbers.Name, addNumbers, d.addNumbers)
+	if err != nil {
+		return nil, err
+	}
+
+	// helloIn1Minute action
+	err = actionManager.RegisterAction(ctx, helloIn1Minute.Name, helloIn1Minute, d.helloIn1Minute)
+	if err != nil {
+		return nil, err
+	}
+
+	return actionManager, nil
+}
+
+func (d *Demo) addNumbers(ctx context.Context, args *structpb.Struct) (*structpb.Struct, annotations.Annotations, error) {
+	number1, ok := args.Fields["number1"]
+	if !ok {
+		return nil, nil, fmt.Errorf("missing required argument number1")
+	}
+	number2, ok := args.Fields["number2"]
+	if !ok {
+		return nil, nil, fmt.Errorf("missing required argument number2")
+	}
+
+	if number1.GetNumberValue() > 100 || number2.GetNumberValue() > 100 {
+		return nil, nil, fmt.Errorf("numbers must be less than 100")
+	}
+
+	sum := number1.GetNumberValue() + number2.GetNumberValue()
+	response := &structpb.Struct{
+		Fields: map[string]*structpb.Value{
+			"sum": structpb.NewNumberValue(sum),
+		},
+	}
+	return response, nil, nil
+}
+
+func (d *Demo) helloIn1Minute(ctx context.Context, args *structpb.Struct) (*structpb.Struct, annotations.Annotations, error) {
+	name, ok := args.Fields["name"]
+	if !ok {
+		return nil, nil, fmt.Errorf("missing required argument name")
+	}
+
+	// Sleep for 1 minute
+	time.Sleep(1 * time.Minute)
+
+	response := &structpb.Struct{
+		Fields: map[string]*structpb.Value{
+			"hello": structpb.NewStringValue("Hello, " + name.GetStringValue() + "!"),
+		},
+	}
+	return response, nil, nil
+}
