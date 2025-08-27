d383cad937ebb19a35eebcef919d390ecb96954b
Geoff Greer <geoff@greer.fm>
2025-08-27 15:27:01 -0700

    Cleanup.
    - Upgrade baton-sdk.
    - Clean up CI workflows.
    - Use github workflow for releases.

--- vendor/github.com/conductorone/baton-sdk/pkg/dotc1z/c1file.go
+++ vendor/github.com/conductorone/baton-sdk/pkg/dotc1z/c1file.go
@@ -3,6 +3,7 @@ package dotc1z
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -149,6 +150,14 @@ func NewC1ZFile(ctx context.Context, outputFilePath string, opts ...C1ZOption) (
 	return c1File, nil
 }
 
+func cleanupDbDir(dbFilePath string, err error) error {
+	cleanupErr := os.RemoveAll(filepath.Dir(dbFilePath))
+	if cleanupErr != nil {
+		err = errors.Join(err, cleanupErr)
+	}
+	return err
+}
+
 // Close ensures that the sqlite database is flushed to disk, and if any changes were made we update the original database
 // with our changes.
 func (c *C1File) Close() error {
@@ -157,7 +166,7 @@ func (c *C1File) Close() error {
 	if c.rawDb != nil {
 		err = c.rawDb.Close()
 		if err != nil {
-			return err
+			return cleanupDbDir(c.dbFilePath, err)
 		}
 	}
 	c.rawDb = nil
@@ -167,17 +176,11 @@ func (c *C1File) Close() error {
 	if c.dbUpdated {
 		err = saveC1z(c.dbFilePath, c.outputFilePath)
 		if err != nil {
-			return err
+			return cleanupDbDir(c.dbFilePath, err)
 		}
 	}
 
-	// Cleanup the database filepath. This should always be a file within a temp directory, so we remove the entire dir.
-	err = os.RemoveAll(filepath.Dir(c.dbFilePath))
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return cleanupDbDir(c.dbFilePath, err)
 }
 
 // init ensures that the database has all of the required schema.
@@ -297,4 +300,28 @@ func (c *C1File) OutputFilepath() (string, error) {
 		return "", fmt.Errorf("c1file: output file path is empty")
 	}
 	return c.outputFilePath, nil
+}
+
+func (c *C1File) AttachFile(other *C1File, dbName string) (*C1FileAttached, error) {
+	_, err := c.db.Exec(`ATTACH DATABASE ? AS ?`, other.dbFilePath, dbName)
+	if err != nil {
+		return nil, err
+	}
+
+	return &C1FileAttached{
+		safe: true,
+		file: c,
+	}, nil
+}
+
+func (c *C1FileAttached) DetachFile(dbName string) (*C1FileAttached, error) {
+	_, err := c.file.db.Exec(`DETACH DATABASE ?`, dbName)
+	if err != nil {
+		return nil, err
+	}
+
+	return &C1FileAttached{
+		safe: false,
+		file: c.file,
+	}, nil
 }
