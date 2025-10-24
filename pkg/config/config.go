cddfe0fa0880a9d7320dfb1493b581540edfed76
MJ Palanker <mj.palanker@conductorone.com>
2025-10-23 18:18:42 -0700

    cleanup args

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -27,9 +27,27 @@ var (
 	)
 	InitDB                  = field.BoolField("init-db", field.WithDescription("Whether to initialize the database."), field.WithDefaultValue(false))
 	DbFileName              = field.StringField("db-file-name", field.WithDescription("The name of the database file."), field.WithDefaultValue("baton-demo.db"))
-	ResourceDropProbability = field.IntField("resource-drop-probability", field.WithDescription("The probability we should drop any given resource from a sync, int from 0 - 100"), field.WithDefaultValue(0))
-	RTDropProbability       = field.IntField("resource-type-drop-probability", field.WithDescription("The probability we should drop any whole resource type from the sync, int from 0 - 100"), field.WithDefaultValue(0))
-	GrantDropProbability    = field.IntField("grant-drop-probability", field.WithDescription("The probability we should drop any grant from the sync, int from 0 - 100"), field.WithDefaultValue(0))
+	ResourceDropProbability = field.IntField(
+		"resource-drop-probability",
+		field.WithDescription("The probability we should drop any given resource from a sync, int from 0 - 100"),
+		field.WithDefaultValue(0),
+		field.WithInt(func(r *field.IntRuler) { r.Gt(0) }),
+		field.WithInt(func(r *field.IntRuler) { r.Lt(101) }),
+	)
+	RTDropProbability = field.IntField(
+		"resource-type-drop-probability",
+		field.WithDescription("The probability we should drop any whole resource type from the sync, int from 0 - 100"),
+		field.WithDefaultValue(0),
+		field.WithInt(func(r *field.IntRuler) { r.Gt(0) }),
+		field.WithInt(func(r *field.IntRuler) { r.Lt(101) }),
+	)
+	GrantDropProbability = field.IntField(
+		"grant-drop-probability",
+		field.WithDescription("The probability we should drop any grant from the sync, int from 0 - 100"),
+		field.WithDefaultValue(0),
+		field.WithInt(func(r *field.IntRuler) { r.Gt(0) }),
+		field.WithInt(func(r *field.IntRuler) { r.Lt(101) }),
+	)
 )
 
 var relationships = []field.SchemaFieldRelationship{}
